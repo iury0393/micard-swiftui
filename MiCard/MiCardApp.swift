20c6b188a1e426e3d1c78d4a7dca351422e1d60a
Iury Vasconcelos <iury0393@gmail.com>
2022-08-17 14:26:43 -0300

    first commit

---
+++ MiCard/MiCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiCardApp.swift
+//  MiCard
+//
+//  Created by Iury Vasconcelos on 17/08/22.
+//
+
+import SwiftUI
+
+@main
+struct MiCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
