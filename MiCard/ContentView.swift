5e83f2191dbfac152d3fedd35efb10ed71e2460d
Iury Vasconcelos <iury0393@gmail.com>
2022-08-17 15:12:30 -0300

    Finalizando projeto

--- MiCard/ContentView.swift
+++ MiCard/ContentView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("profile")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+                    .frame(width: 150.0, height: 150.0)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                Text("Iury Vasconcelos")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+55 (85) 9 1929-3949", imageName: "phone.fill")
+                InfoView(text: "iruy0393@gmail.com", imageName: "envelope.fill")
+            }
+            
+        }
     }
 }
 
---
+++ MiCard/InfoView.swift
@@ -0,0 +1,32 @@
+//
+//  InfoView.swift
+//  MiCard
+//
+//  Created by Iury Vasconcelos on 17/08/22.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 40)
+            .overlay(HStack {
+                Image(systemName: imageName).foregroundColor(.green)
+                Text(text)
+            })
+            .padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
